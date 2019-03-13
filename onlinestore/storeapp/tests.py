3a235ef62f8a7228c465e9b01f7faeda9dce9be5
Derek Hendrick <phantom.dev.elb@gmail.com>
2019-03-13 16:07:15 -0700

    Update tests.py

--- onlinestore/storeapp/tests.py
+++ onlinestore/storeapp/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from .models import ProductTypes, Products, Orders, Reviews
+from .django.urls import reverse
 
 # Create your tests here.
+class ProductTypesTest(TestCase):
+    def test_stringOutput(self):
+        producttype = ProductTypes(typename = 'Test Product Type')
+        self.assertEqual(str(producttype), producttype.typename)
+    def test_tablename(self):
+        self.assertEqual(str(ProductTypes._meta.db_table), 'producttype')
+        
+class ProductTest(TestCase):
+    def test_stringOutput(self):
+        product = Products(productname = 'Test Product Title')
+        self.assertEqual(str(product), product.productname)
+    def test_tablename(self):
+        self.assertEqual(str(Products._meta.db_table), 'product')
+
+class OrderTest(TestCase):
+    def test_stringOutput(self):
+        order = Orders(id = '1')
+        self.assertEqual(str(order), order.id)
+    def test_tablename(self):
+        self.assertEqual(str(Orders._meta.db_table), 'order')
+        
+class ReviewTest(TestCase):
+    def test_stringOutput(self):
+        review = Reviews(id = '1')
+        self.assertEqual(str(review), review.id)
+    def test_tablename(self):
+        self.assertEqual(str(Reviews._meta.db_table), 'review')
+        
+ #Testing a view
+
+class TestIndex(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'storeapp/index.html')
+        
+class TestProductTypes(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('getproducttypes'))
+        self.assertEqual(response.status_code, 200)
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('getproducttypes'))
+        self.assertTemplateUsed(response, 'storeapp/getproducttypes.html')
+        
+class TestNewOrder(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('neworder'))
+        self.assertEqual(response.status_code, 200)
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('neworder'))
+        self.assertTemplateUsed(response, 'storeapp/neworder.html')
